6b30ee8b5e3aa7ffe2c5a6f8ab8d9c06aee4d48d
Piotr Miśkiewicz <piotr.miskiewicz.priv@gmail.com>
2020-09-12 01:24:32 +0200

    Updates

---
+++ precache-manifest.a91f91e631de090b28996807102ca503.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a688524a6910c3b0fb5ca37e3c16d9",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "73c57de87a6c28e6198f",
+    "url": "/shop/static/css/main.9a6b7cec.chunk.css"
+  },
+  {
+    "revision": "33427e7a78b2e65500c1",
+    "url": "/shop/static/js/2.ee773ed3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shop/static/js/2.ee773ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c57de87a6c28e6198f",
+    "url": "/shop/static/js/main.d164066e.chunk.js"
+  },
+  {
+    "revision": "aa1945b71dc1f6fb53f2",
+    "url": "/shop/static/js/runtime-main.4a8e269c.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/shop/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/shop/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/shop/static/media/header3.d9d36b65.jpg"
+  }
+]);
